9a944cbee85e06f0f087a66752f1fd4c9cf5ccf8
jcassola <jcassola96@gmail.com>
2023-05-02 16:48:32 -0400

    add from's address to email's text

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.post("/contact-me", function (req, res) {
     from,
     to,
     subject,
-    text: `${text}\n Sent by ${from}`,
+    text: `${text}\nSent by ${from}`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
